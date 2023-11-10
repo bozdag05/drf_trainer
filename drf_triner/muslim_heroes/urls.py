914c9511cb2d9635f8096e9aa2baf3aa870c3429
bozdag05 <muhammadosmanov02@gmail.com>
2023-11-10 22:19:01 +0300

    add path 'auth-heroes' in urls.py

--- drf_triner/muslim_heroes/urls.py
+++ drf_triner/muslim_heroes/urls.py
@@ -8,6 +8,7 @@ from rest_framework import routers
 
 urlpatterns = [
     #path("api/v1/", include(router.urls), name="heroes_list_crud")
+    path("api/v1/auth-heroes/", include("rest_framework.urls")),
     path("api/v1/heroes/", HeroesAPIViewList.as_view(), name="heroes"),
     path("api/v1/heroes/<int:pk>/", HeroesAPIViewUpdate.as_view(), name="hero_update"),
     path("api/v1/hero_delete/<int:pk>/", HeroesAPIViewDestroy.as_view(), name="hero_destroy"),
